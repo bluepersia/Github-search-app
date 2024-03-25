2b3b154de8c935142b7a2e16c8aa750b5617bf8f
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-03-25 13:07:28 -0600

    Mobile style

--- src/components/SearchApp.tsx
+++ src/components/SearchApp.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import SearchBar from './SearchBar';
-import styles from './SearchBar.module.css';
+import styles from './SearchApp.module.css';
 import Toggle from './Toggle';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { IUser } from '../models/user';
 import User from './User';
+import { AppContext } from '../App';
 
 export default function SearchApp(): JSX.Element {
   const [search, setSearch] = useState<string>('');
@@ -15,6 +16,8 @@ export default function SearchApp(): JSX.Element {
     isLoading,
     error,
   } = useQuery({ queryKey: ['search'], queryFn: fetchUser });
+
+  const { mode } = useContext(AppContext);
 
   async function fetchUser(): Promise<IUser | null> {
     if (!search) return null;
@@ -31,14 +34,18 @@ export default function SearchApp(): JSX.Element {
   }, [search]);
 
   return (
-    <div className={styles.searchApp}>
+    <div className={styles.searchApp + ' ' + styles[mode.toString()]}>
       <header className={styles.header}>
-        <h1 className={styles.logo}>devfinder</h1>
-        <Toggle />
+        <div className={styles.header_top}>
+          <h1 className={styles.logo}>devfinder</h1>
+          <Toggle />
+        </div>
+        <SearchBar setSearch={setSearch} error={error ? 'No results' : ''} />
       </header>
-      <SearchBar setSearch={setSearch} error={error ? 'No results' : ''} />
-      {isLoading && <h2>Loading...</h2>}
-      {user && <User user={user} />}
+      <main className={styles.main}>
+        {isLoading && <h2>Loading...</h2>}
+        {user && <User user={user} />}
+      </main>
     </div>
   );
 }
